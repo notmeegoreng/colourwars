40260e5a665285594b339c3e646253892370f1c9
notmeegoreng <notmeegoreng@gmail.com>
2026-05-12 23:53:36 +0800

    add player counts

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(Default, Copy, Clone)]
+#[derive(Copy, Clone)]
 pub struct Cell {
     // 2 lsb: count - 1
     // rest: id of player
@@ -12,94 +12,129 @@ impl fmt::Debug for Cell {
         write!(
             fmt,
             "Cell {{ id: {}, count: {} }}",
-            (self.val & !0b11) >> 2,
+            self.get_player(),
             self.count()
         )?;
         Ok(())
     }
 }
 
+impl Default for Cell {
+    fn default() -> Self {
+        Self { val: Self::BLANK }
+    }
+}
+
 impl Cell {
+    const BLANK: u8 = u8::MAX;
+
+    fn is_empty(&self) -> bool {
+        self.val == Self::BLANK
+    }
+
     fn count(&self) -> u8 {
-        if self.val == 0 {
+        if self.is_empty() {
             0
         } else {
             (self.val & 0b11) + 1
         }
     }
 
-    fn inc(&mut self, id: u8) -> bool {
-        if self.val == 0 {
-            self.val = id;
+    // set cell value to 3
+    pub fn set(&mut self, p: u8, cs: &mut [usize]) {
+        assert!(self.is_empty());
+        cs[p as usize] += 1;
+        self.val = (p << 2) | 0b10;
+    }
+
+    fn inc(&mut self, p: u8, cs: &mut [usize]) -> bool {
+        if self.is_empty() {
+            self.val = p << 2;
+            cs[p as usize] += 1;
             false
         } else {
             let c = self.val & 0b11;
             if c == 0b11 {
-                // already popping, ignore
+                // already popping, should be same player, ignore
                 return false;
             }
-            self.val = (c + 1) | id;
+            let cp = self.get_player();
+            if cp == p {
+                self.val += 1;
+            } else {
+                cs[cp as usize] -= 1;
+                cs[p as usize] += 1;
+                self.val = (c + 1) | (p << 2);
+            }
             c == 0b10
         }
     }
 
-    // set cell value to 3
-    pub fn set(&mut self, id: u8) {
-        self.val = id | 0b10;
-    }
-
     fn clear(&mut self) {
-        self.val = 0;
+        self.val = Self::BLANK;
     }
 
-    fn check(&self, id: u8) -> bool {
-        self.val & !0b11 == id
+    fn get_player(&self) -> u8 {
+        if self.is_empty() {
+            0
+        } else {
+            (self.val & !0b11) >> 2
+        }
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct Board<const R: usize, const N: usize> {
+pub struct Board<const R: usize, const N: usize, const P: usize> {
     // row major order
-    pub board: [Cell; N],
+    board: [Cell; N],
+    pub player_counts: [usize; P],
 }
 
-impl<const R: usize, const N: usize> Default for Board<R, N> {
+impl<const R: usize, const N: usize, const P: usize> Default for Board<R, N, P> {
     fn default() -> Self {
         Board {
             board: [Cell::default(); N],
+            player_counts: [0; P],
         }
     }
 }
 
-impl<const R: usize, const N: usize> Board<R, N> {
-    fn inc(&mut self, idx: usize, id: u8, v: &mut Vec<usize>) {
-        if self.board[idx].inc(id) {
+impl<const R: usize, const N: usize, const P: usize> Board<R, N, P> {
+    pub fn set(&mut self, idx: usize, player: u8) {
+        self.board[idx].set(player, &mut self.player_counts);
+    }
+
+    fn inc(&mut self, idx: usize, player: u8, v: &mut Vec<usize>) {
+        if self.board[idx].inc(player, &mut self.player_counts) {
             v.push(idx);
         }
     }
 
-    pub fn inc_all(&mut self, idx: usize, id: u8) {
+    pub fn inc_all(&mut self, idx: usize, player: u8) {
         // next to pop
         let mut c: Vec<usize> = vec![];
-        self.inc(idx, id, &mut c);
+        self.inc(idx, player, &mut c);
         while !c.is_empty() {
             let mut n = vec![];
+            let r = c.len();
             for idx in c {
                 self.board[idx].clear();
                 let i = idx % R;
                 if i != 0 {
-                    self.inc(idx - 1, id, &mut n);
+                    self.inc(idx - 1, player, &mut n);
                 }
                 if idx >= R {
-                    self.inc(idx - R, id, &mut n);
+                    self.inc(idx - R, player, &mut n);
                 }
                 if i != R - 1 {
-                    self.inc(idx + 1, id, &mut n);
+                    self.inc(idx + 1, player, &mut n);
                 }
                 if idx + R < N {
-                    self.inc(idx + R, id, &mut n)
+                    self.inc(idx + R, player, &mut n)
                 }
             }
+            // remove popped
+            self.player_counts[player as usize] -= r;
             c = n;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod board;
 mod search;
 
 pub fn main() {
-    let mut s: board::Board<3, 9> = board::Board::default();
+    let mut s: board::Board<3, 9, 2> = board::Board::default();
     let i = s.index(1, 1);
-    s.board[i].set(0b100);
-    s.inc_all(i, 0b100);
+    s.set(i, 0);
+    s.inc_all(i, 0);
     println!("{:?}", s);
 }
